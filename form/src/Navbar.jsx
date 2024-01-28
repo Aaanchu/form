f30eb7f82cf7122913420ea4ea0ffcd626053102
Aanchal Kumari <88676507+Aaanchu@users.noreply.github.com>
2024-01-28 21:22:14 +0530

    Navbar.jsx

--- form/src/Navbar.jsx
+++ form/src/Navbar.jsx
@@ -6,8 +6,6 @@ const Navbar=()=>{
         <nav className='navbar'>
             <ul className='navbar ul'>
                 <li className='navbar il'><a className='navlink' href='/Navbar.jsx'>Home</a></li>
-                <li className='navbar il'><a className='navlink' href='/about'>About</a></li>
-                <li className='navbar il'><a className='navlink' href='/contact'>Contact</a></li>
             </ul>
         </nav>
     );
